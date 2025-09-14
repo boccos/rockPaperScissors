4258461f3f0e92baea6d94e0f866f60d8e1863db
boccos <145885073+boccos@users.noreply.github.com>
2025-09-14 01:00:57 -0400

    Added very necessary emojis to the displayed text

--- rps.js
+++ rps.js
@@ -12,7 +12,7 @@ let score = document.querySelector("#scores");
 let result = document.querySelector("#gameResult");
 
 const restartGame = document.createElement("button");
-restartGame.textContent = "Restart";
+restartGame.textContent = "Restart 🔄";
 
 buttonRock.addEventListener("click", () => {
     buttons.forEach(b => b.style.backgroundColor = "#333");
